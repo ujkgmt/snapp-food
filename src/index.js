83cd628d1fc512aec292ef2546d40b6dbc35853c
Amir <ujkgmt@gmail.com>
2021-11-02 16:27:11 +0330

    addFirebase

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import { render } from 'react-dom'
 
 import App from "./App"
 
+
+// import { initializeApp } from "firebase/app";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDgferxTSQfMsY6E9A_T8IHJ12fZYa8TvA",
+//   authDomain: "snapp-food-e8aac.firebaseapp.com",
+//   projectId: "snapp-food-e8aac",
+//   storageBucket: "snapp-food-e8aac.appspot.com",
+//   messagingSenderId: "302924554422",
+//   appId: "1:302924554422:web:e972de9d52ae653750c617"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+
 render(<App/>, document.getElementById("root"))
